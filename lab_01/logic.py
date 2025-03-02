f256e501a868a0c3c98e6a7a8d5351a2132cbf15
Pricolistok <dokolin05@gmail.com>
2025-03-02 20:07:14 +0300

    add point

--- lab_01/logic.py
+++ lab_01/logic.py
@@ -1,4 +1,3 @@
-from utils import read_from_file
 from logic_of_newton_polynomial import cnt_newton_polynomial
 from logic_of_hermite_polynomial import cnt_hermite_interpolation
 from utils import read_from_file
@@ -22,12 +21,23 @@ def diff_data(table, x, y, y_derivative):
         y_derivative.append(i[2])
 
 
-def get_table():
+def get_table_with_one_level():
     table = []
     read_from_file(table)
     x_values, y_values, y_derivative = [], [], []
     diff_data(table, x_values, y_values, y_derivative)
+    level_of_polynomial = int(input("Введите степень полинома для нахождения значения: "))
+    x_finder = float(input("Введите X для подсчета значения: "))
+    result_newton = cnt_newton_polynomial(x_values, y_values, x_finder, level_of_polynomial)
+    result_hermite = cnt_hermite_interpolation(table, level_of_polynomial, x_finder)
+    print('Результат полученный при помощи полинома Ньютона: {:10.5f}\nРезультат полученный при помощи полинома Эрмита: {:10.5f}'.format(result_newton, result_hermite))
+
+
+def get_table_with_more_levels():
+    table = []
     read_from_file(table)
+    x_values, y_values, y_derivative = [], [], []
+    diff_data(table, x_values, y_values, y_derivative)
     level_of_polynomial = list(map(int, input("Введите степени полинома для таблицы: ").split()))
     x_finder = float(input("Введите X для подсчета значения: "))
     print_header_of_table(x_finder)
@@ -35,3 +45,38 @@ def get_table():
         result_newton = cnt_newton_polynomial(x_values, y_values, x_finder, i)
         result_hermite = cnt_hermite_interpolation(table, i, x_finder)
         print_row_of_table(i, result_newton, result_hermite)
+
+
+def find_root_with_newton_polynomial(level_of_polynomial, x_values, y_values):
+    low, high = x_values[0], x_values[-1]
+    while high - low > 1e-6:
+        middle = (low + high) / 2
+        value = cnt_newton_polynomial(x_values, y_values, middle, level_of_polynomial)
+        if value > 0:
+            high = middle
+        else:
+            low = middle
+    return (low + high) / 2
+
+
+def find_root_with_hermite_polynomial(level_of_polynomial, low, high, table):
+    while high - low > 1e-6:
+        middle = (low + high) / 2
+        value = cnt_hermite_interpolation(table, level_of_polynomial, middle)
+        if value > 0:
+            high = middle
+        else:
+            low = middle
+    return (low + high) / 2
+
+
+
+def find_roots():
+    level_of_polynomial = int(input("Введите степень полинома для поиска: "))
+    table = []
+    read_from_file(table)
+    x_values, y_values, y_derivative = [], [], []
+    diff_data(table, x_values, y_values, y_derivative)
+    result_newton = find_root_with_newton_polynomial(level_of_polynomial, x_values, y_values)
+    result_hermite = find_root_with_hermite_polynomial(level_of_polynomial, x_values[0], x_values[-1], table)
+    print('Корень полученный при помощи полинома Ньютона: {:10.5f}\nКорень полученный при помощи полинома Эрмита: {:10.5f}'.format(result_newton, result_hermite))
--- lab_01/main.py
+++ lab_01/main.py
@@ -1,18 +1,25 @@
-from logic import get_table
+from logic import get_table_with_one_level, get_table_with_more_levels, find_roots
 
 
 def print_menu():
-    print("1 - Вывести таблицу занчений")
+    print("1 - Вывести таблицу занчений по заданному N и X")
+    print("2 - Вывести таблицу занчений по заданному множеству N и единаственному X")
+    print("3 - Найти корень уровнения при помощи обратной интерполяции")
 
 
 def main():
     print_menu()
     choice = int(input("Ваш выбор: "))
-    match choice != 0:
+    match choice:
         case 1:
-            get_table()
+            get_table_with_one_level()
+        case 2:
+            get_table_with_more_levels()
+        case 3:
+            find_roots()
         case _:
             print("Ошибка!")
+
 
 if __name__ == "__main__":
     main()
